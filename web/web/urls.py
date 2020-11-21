d08f6d6a887216f42374c1e42bb25fcea6b206f1
pratik9779 <59467572+pratik9779@users.noreply.github.com>
2020-11-21 10:46:48 +0530

    Update urls.py

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "Softtech Admin"
+admin.site.site_title = "Softtech Admin Portal"
+admin.site.index_title = "Welcome to Softtech"
+
 urlpatterns = [
     path('', include('mysite.urls')),
     path('admin/', admin.site.urls),
